e6280003c751743d5a72d500c62d76028ee916aa
sarin yangsom <ett0088@gmail.com>
2019-03-23 18:14:13 +0700

    mod app.js

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-var express = require('express')
-var app = express()
-var fs = require('fs')
-var ejs = require('ejs')
+var express = require('express');
+var fs = require('fs');
+var ejs = require('ejs');
 
+var app = express();
 //Render .html
-app.engine('html',ejs.renderFile)
+app.engine('html',ejs.renderFile);
 //Open port
-const port = 80
+const port = 80;
 app.listen(port, () => {
  console.log('Server working...')
-})
+});
 //Use Bootstrap
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 
 //Route mian
 app.get('/',(req, res) => 
  res.render('home.html')
-)
-app.use((res, req) => {
+);
+app.use((req, res) => {
  res.status(404).render('error.html')
  console.log('>>Error page not found.')
-})
+});
 
 //Set static
-app.use(express.static('views'))
+app.use(express.static('views'));
