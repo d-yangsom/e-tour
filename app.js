0913c48dfc15016ee545717c0a5062297f3fb42c
sarin yangsom <ett0088@gmail.com>
2019-03-24 15:24:15 +0700

    initial add jquery

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const port = 80;
 app.listen(port, () => {
  console.log('Server working...')
 });
-//Use Bootstrap
+//Use Bootstrap + Jquery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
-
+app.use('/dist', express.static(__dirname + '/node_modules/jquery/dist'))
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
 //Route mian
 app.get('/',(req, res) => 
  res.render('home.html')
