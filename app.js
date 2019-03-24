fb17decf36db009e42ea546be31ab7850c3f1b0c
sarin yangsom <ett0088@gmail.com>
2019-03-24 16:08:51 +0700

    mod app

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.listen(port, () => {
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
 app.use('/dist', express.static(__dirname + '/node_modules/jquery/dist'))
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
-app.use('/node_modules', express.static(__dirname + '/node_modules'))
+app.use('/dist', express.static(__dirname + '/node_modules/popper.js/dist'))
 //Route mian
 app.get('/',(req, res) => 
  res.render('home.html')
